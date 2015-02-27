46f9f3d858e98c3aff2ae6645e38df43f89499bb
ctrowat <ctrowat+github@gmail.com>
2015-02-27 11:47:57 -0700

    work in progress
    Working on dashboard-ey stuff

--- site/pages/index.js
+++ site/pages/index.js
@@ -2,8 +2,8 @@ var app = angular.module('demo', ['CornerCouch'])
 .controller('brewberryController', function($scope, cornercouch) {
   $scope.id = "pi";
   $scope.server = cornercouch();
-  $scope.server.uri = "http://localhost:5984";
-  $scope.server.method = "JSONP";
+  $scope.server.uri = "http://localhost:3000/db";
   $scope.server.getDB('brewberry_index').getInfo().success(function(a) { console.dir(a); });
+  $scope.brews = [{id:"testbrew", name:"something awesome"}];
 });
 angular.bootstrap(document, ['demo']);
