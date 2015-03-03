3442e78cbd5e1da02678fb66e579dc9c2275afa4
Chris Rowar <ctrowat+github@gmail.com>
2015-03-03 14:42:42 +0000

    HSV support for the RGB LED

--- daemon/app.js
+++ daemon/app.js
@@ -34,6 +34,31 @@ var storedTemps = {};
 var stop = false;
 var ledState = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]];
 
+var HSVToRGB = function (h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (h && s === undefined && v === undefined) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.floor(r * 255),
+        g: Math.floor(g * 255),
+        b: Math.floor(b * 255)
+    };
+};
+
 var outputLEDState = function() {
   var bitArray = new Array(144);
   for (var i = 0;i < 6;i++) {
@@ -192,9 +217,11 @@ var getActiveBrews = function() {
         }
       });
       // update LED state as appropriate
-      for (var i = 0;i < 3;i++) {
-        ledState[piLed][i] = Math.floor(Math.random() * 256);
-      }
+      var rgb = HSVToRGB(Math.random(),1,0.05);
+      ledState[piLed][0] = rgb.r;
+      ledState[piLed][1] = rgb.g;
+      ledState[piLed][2] = rgb.b;
+
       if (!stop) { 
         setTimeout(getActiveBrews, collectInterval); 
       }      
